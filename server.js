ceb3d4836e1cf2037a540097466f5142a6ec5f10
Stuart Wong <swong31@gmail.com>
2020-09-14 13:46:53 -0700

    Tables are now being pushed into arrays, but table.html is displaying objects as undefined

--- server.js
+++ server.js
@@ -12,88 +12,55 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const tables = [
-    {
-      routeName: 1,
-      id: "",
-      name: "",
-      email: "",
-      phone: ""
-    },
-    {
-        routeName: 2,
-        id: "",
-        name: "",
-        email: "",
-        phone: ""
-      },
-      {
-        routeName: 3,
-        id: "",
-        name: "Jedi Master",
-        email: "",
-        phone: ""
-      },
-      {
-        routeName: 4,
-        id: "",
-        name: "Jedi Master",
-        email: "",
-        phone: ""
-      },
-      {
-        routeName: 5,
-        id: "",
-        name: "Jedi Master",
-        email: "",
-        phone: ""
-      },
-]
-
-const waitlist = []
+// Tables to be reserved; max length is 5
+const tables = [];
+// Waitlist if tables are full
+const waitlist = [];
 
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 // Sends user to tables page
-  app.get("/table", function(req, res) {
-    res.sendFile(path.join(__dirname, "table.html"));
-  });
+app.get("/table", function (req, res) {
+  res.sendFile(path.join(__dirname, "table.html"));
+});
 // Sends user to reserve page
-  app.get("/reserve", function(req, res) {
-    res.sendFile(path.join(__dirname, "reserve.html"));
-  });
-  
- // Displays all tables
-app.get("/api/tables", function(req, res) {
-    return res.json(tables);
-  });
+app.get("/reserve", function (req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
 
-app.get("/api/waitlist"), function(req, res) {
-    return res.json(waitlist)
-}
-  
-  
-  // takes in JSON input
-  app.post("/api/tables", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    let table = req.body;
-  
-    
-    
-  
-    console.log(table);
-  
+// Displays all tables
+app.get("/api/tables", function (req, res) {
+  res.json(tables);
+});
 
-  
-    res.json(table);
-  });
-  
-  // Starts the server to begin listening
-  // =============================================================
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
+app.get("/api/waitlist", (req, res) => res.json(waitlist));
+
+// takes in JSON input
+app.post("/api/tables", function (req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+
+  if (tables.length < 5) {
+    tables.push(req.body);
+    res.json(true);
+  } else {
+    waitlist.push(req.body);
+    res.json(false);
+  }
+});
+
+app.post("/api/clear", function(req, res) {
+  // Empty out the arrays of data
+  tables.length = 0;
+  waitlist.length = 0;
+
+  res.json({ ok: true });
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
